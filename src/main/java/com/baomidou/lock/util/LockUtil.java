da7dadbf898372843161bf7d189079151c64a71e
zzh <qzyule@qq.com>
2019-05-17 11:12:21 +0800

    fix: 修复lockUtil getMac 异常，当获取异常时，使用UUID作为MAC地址

--- src/main/java/com/baomidou/lock/util/LockUtil.java
+++ src/main/java/com/baomidou/lock/util/LockUtil.java
@@ -1,12 +1,12 @@
 /**
  * ﻿Copyright © 2018 organization 苞米豆
- *
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -18,6 +18,7 @@ package com.baomidou.lock.util;
 import java.lang.management.ManagementFactory;
 import java.net.InetAddress;
 import java.net.NetworkInterface;
+import java.util.UUID;
 
 /**
  * 分布式锁工具类
@@ -33,12 +34,13 @@ public class LockUtil {
      * @return macAddress
      */
     public static String getLocalMAC() {
+        String localMac;
         try {
             InetAddress ia = InetAddress.getLocalHost();
             // 获得网络接口对象（即网卡），并得到mac地址，mac地址存在于一个byte数组中。
             byte[] mac = NetworkInterface.getByInetAddress(ia).getHardwareAddress();
             // 下面代码是把mac地址拼装成String
-            StringBuilder sb = new StringBuilder();
+            StringBuffer sb = new StringBuffer();
             for (int i = 0; i < mac.length; i++) {
                 if (i != 0) {
                     sb.append("-");
@@ -48,10 +50,12 @@ public class LockUtil {
                 sb.append(s.length() == 1 ? 0 + s : s);
             }
             // 把字符串所有小写字母改为大写成为正规的mac地址并返回
-            return sb.toString().toUpperCase().replace("-", "");
+            localMac = sb.toString();
         } catch (Exception e) {
-            throw new IllegalStateException("getLocalMAC error");
+            //TODO 获取mac地址应该要做到最大化兼容
+            localMac = UUID.randomUUID().toString();
         }
+        return localMac.toUpperCase().replaceAll("-", "");
     }
 
     /**
@@ -63,7 +67,8 @@ public class LockUtil {
         String pid = ManagementFactory.getRuntimeMXBean().getName();
         int indexOf = pid.indexOf('@');
         if (indexOf > 0) {
-            return pid.substring(0, indexOf);
+            pid = pid.substring(0, indexOf);
+            return pid;
         }
         throw new IllegalStateException("ManagementFactory error");
     }
