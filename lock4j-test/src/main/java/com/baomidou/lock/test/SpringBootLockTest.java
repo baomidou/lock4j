17f3e97a16734267686eef281f18cda28c84e625
zengzhihong <63976799@qq.com>
2022-02-24 10:24:10 +0800

    update: copyright

--- lock4j-test/src/main/java/com/baomidou/lock/test/SpringBootLockTest.java
+++ lock4j-test/src/main/java/com/baomidou/lock/test/SpringBootLockTest.java
@@ -1,5 +1,5 @@
 /*
- *  Copyright (c) 2018-2021, baomidou (63976799@qq.com).
+ *  Copyright (c) 2018-2022, baomidou (63976799@qq.com).
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
