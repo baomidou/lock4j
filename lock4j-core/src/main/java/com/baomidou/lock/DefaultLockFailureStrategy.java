6aa1e1dd614a944e5c616ba2ba1662f51f6d32ea
wanghaiqi <wanghaiqi@beeplay123.com>
2024-02-22 15:01:39 +0800

    ✨ When the lock fails, the error message contains the key

--- lock4j-core/src/main/java/com/baomidou/lock/DefaultLockFailureStrategy.java
+++ lock4j-core/src/main/java/com/baomidou/lock/DefaultLockFailureStrategy.java
@@ -17,20 +17,19 @@
 package com.baomidou.lock;
 
 import com.baomidou.lock.exception.LockFailureException;
-import lombok.extern.slf4j.Slf4j;
 
 import java.lang.reflect.Method;
 
 /**
  * @author zengzhihong
  */
-@Slf4j
 public class DefaultLockFailureStrategy implements LockFailureStrategy {
-
+    
     protected static String DEFAULT_MESSAGE = "request failed,please retry it.";
-
+    
     @Override
     public void onLockFailure(String key, Method method, Object[] arguments) {
-        throw new LockFailureException(DEFAULT_MESSAGE);
+        String message = String.format(DEFAULT_MESSAGE + "key:%s", key);
+        throw new LockFailureException(message);
     }
 }
